b0556f1151f1e36b712a5d792508c37ebc416cba
MethaKinde <meethabeelkinde@gmail.com>
2023-12-19 17:26:49 +0100

    add romance book

--- epibook/src/components/Welcome.jsx
+++ epibook/src/components/Welcome.jsx
@@ -35,6 +35,29 @@ class Welcome extends React.Component {
                         )
                     })}
                 </Row>
+                <Row className="justify-content-center text-center mb-5 text-white" >
+                    <div className="jumbotron jumbotron-fluid">
+                        <h1>Romance</h1>
+                    </div>
+                </Row>
+                <Row className="justify-content-center" >
+                    {books.romance.map((book) => {
+                        return (
+                            <Col md={6} lg={4}>
+                                <Card style={{ width: '18rem' }} className="mb-5">
+                                    <Card.Img variant="top" src={book.img} alt={book.title} style={{ height: '20rem'}}/>
+                                    <Card.Body>
+                                        <Card.Title>{book.title}</Card.Title>
+                                        <Card.Text>
+                                            categoria: {book.category}<br />
+                                            prezzo : {book.price}€
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
             </Container>
         )
     }
